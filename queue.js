9a934e27333e1528dd1828cbc75111a7cfde9c15
DEADB17 <DEADB17@gmail.com>
2015-05-19 15:58:35 -0400

    removes the need for NONE

--- queue.js
+++ queue.js
@@ -2,12 +2,10 @@
 
 var slice = Array.prototype.slice;
 
-function NONE () {}
-
 function set(q, val) {
     var listener, next, ret;
     if (arguments.length > 1) { q.value = val; }
-    if (q.listeners.length > 0 && q.value !== NONE) {
+    if (q.listeners.length > 0) {
         listener = q.listeners.shift();
         if (typeof listener === 'function') {
             next = set.bind(null, q);
@@ -24,15 +22,15 @@ function set(q, val) {
 
 function get(q /*listeners*/) {
     var listeners = q.listeners;
+    var isSettled = listeners.length < 1;
     listeners.push.apply(listeners, slice.call(arguments, 1));
-    q = set(q, q.value);
-    return q;
+    return isSettled ? set(q, q.value) : q;
 }
 
 function create(/*listeners*/) {
     return Object.seal({
         listeners: arguments.length ? slice.call(arguments) : [],
-        value: NONE,
+        value: undefined,
     });
 }
 
